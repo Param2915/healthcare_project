729c7151a86094a52229e3560c12d8e084ce8db2
Param2915 <123792929+Param2915@users.noreply.github.com>
2024-10-22 15:47:31 +0530

    22/10/2024

---
+++ server/server.js
@@ -0,0 +1,27 @@
+//FRAMEWORK CONFIGURATION
+const express = require("express");
+const connectDb=require("./config/dbConnection");
+const errorHandler=require("./middleware/errorHandler");
+const cors=require("cors");
+
+//env file config
+const dotenv =require("dotenv");
+dotenv.config();
+
+connectDb();
+const app=express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+//error handling middleware
+app.use(errorHandler);
+
+//ROUTES BELOW
+app.get('/',(req,res)=>{
+    res.send("Working");
+});
+
+
+//APP CONFIG START
+app.listen
